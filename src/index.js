6d9aa40e7d7ae87ff25c8575e39c6cf15db9409a
Wilfredo Morales <wiljmoralesa@gmail.com>
2021-12-04 11:25:38 -0400

    first version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const urlSettings = "&appid=18cca4b95529ce31913b2c6dad32192c&lang=es&units=metric";
 
-console.log('Happy hacking :)')
+const button = document.getElementById("submit");
+const input = document.querySelector("input");
+const mountNode = document.querySelector("#app")
+
+async function fetchWeather(city) {
+  const response = await window.fetch(baseUrl + city + urlSettings);
+  const data = await response.json();
+  return data;
+}
+
+const printWheather = async () => {
+  const city = input.value
+  const myWheather = await fetchWeather(city);
+  const whaterImage = document.createElement('img')
+  whaterImage.src = `https://openweathermap.org/img/w2/${myWheather.weather[0].icon}.png`
+  whaterImage.width= '75'
+  const container = document.createElement('div')
+  container.className = 'flex flex-col items-center bg-green-100 w-3/12  m-5 p-10 rounded-lg'
+  const cityName = document.createElement('h2')
+  cityName.className = 'font-bold text-2xl'
+  cityName.textContent = myWheather.name
+  const temp = document.createElement('p')
+  temp.textContent = `${myWheather.main.temp}°C`
+  const cityWheaher = document.createElement('p')
+  cityWheaher.textContent = myWheather.weather[0].description
+  container.append(cityName, whaterImage,cityWheaher,temp)
+  mountNode.append(container)
+  input.value = ''
+};
+
+
+button.addEventListener('click', printWheather)
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === 'Enter') {
+    printWheather()
+  } 
+});
